a584cac1c5b035bbe6cf7f029245f28e0fd3217f
Peter <pedrolucas9515@gmail.com>
2026-05-24 17:16:56 +0000

    The interactive map is loading with markers, but the map background is c

--- src/app/map/MapComponent.tsx
+++ src/app/map/MapComponent.tsx
@@ -83,8 +83,8 @@ export default function MapComponent() {
       scrollWheelZoom={true}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       
       <ZoomControl position="topright" />
